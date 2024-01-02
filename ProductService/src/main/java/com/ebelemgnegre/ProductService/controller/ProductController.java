dc0c16093affd62df4eed9eb905c6e4e27d70720
EtienneWend-KuuniBel <belemgnegreetienne@gmail.com>
2024-01-02 12:30:53 +0000

    add: reduceQuantity feature in product service

--- ProductService/src/main/java/com/ebelemgnegre/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/ebelemgnegre/ProductService/controller/ProductController.java
@@ -27,4 +27,10 @@ public class ProductController {
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
 
     }
+
+    @PutMapping("/reduceQuantity/{id}")
+    ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity){
+        productService.reduceQuantity(productId, quantity);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- ProductService/src/main/java/com/ebelemgnegre/ProductService/service/ProductService.java
+++ ProductService/src/main/java/com/ebelemgnegre/ProductService/service/ProductService.java
@@ -6,4 +6,6 @@ import com.ebelemgnegre.ProductService.model.ProductResponse;
 public interface ProductService {
     long addProduct(ProductRequest productRequest);
     ProductResponse getProductById(long productId);
+
+    void reduceQuantity(long productId, long quantity);
 }
