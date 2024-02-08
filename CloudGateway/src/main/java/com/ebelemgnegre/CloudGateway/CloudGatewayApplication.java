b9e26012640f15501101181ea49fa8cab6ba60f0
ebelemgnegre <ebelemgnegre@nine7soft.com>
2024-02-08 00:30:45 +0000

    add: spring security with okta auth as provider

--- CloudGateway/src/main/java/com/ebelemgnegre/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/ebelemgnegre/CloudGateway/CloudGatewayApplication.java
@@ -11,18 +11,18 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
 
-	@Bean
-	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
-		return factory -> factory.configureDefault(
-				id -> new Resilience4JConfigBuilder(id)
-						.circuitBreakerConfig(
-								CircuitBreakerConfig.ofDefaults()
-						).build()
-		);
-	}
+    @Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(
+                id -> new Resilience4JConfigBuilder(id)
+                        .circuitBreakerConfig(
+                                CircuitBreakerConfig.ofDefaults()
+                        ).build()
+        );
+    }
 
 }
