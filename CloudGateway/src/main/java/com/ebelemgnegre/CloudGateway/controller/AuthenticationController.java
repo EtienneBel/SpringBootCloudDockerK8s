4492d3ea3b8dc5b114625bef78e45765a6f2cea8
ebelemgnegre <ebelemgnegre@nine7soft.com>
2024-02-26 12:57:09 +0000

    update: readme content

--- CloudGateway/src/main/java/com/ebelemgnegre/CloudGateway/controller/AuthenticationController.java
+++ CloudGateway/src/main/java/com/ebelemgnegre/CloudGateway/controller/AuthenticationController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.stream.Collectors;
 
 @RestController
-    @RequestMapping("/authenticate")
+@RequestMapping("/authenticate")
 public class AuthenticationController {
 
     @GetMapping("/login")
