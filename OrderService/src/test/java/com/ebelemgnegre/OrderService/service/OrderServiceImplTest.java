3c2016f3f7d66c50f6da1fca25ead50a5c68d81b
EtienneWend-KuuniBel <belemgnegreetienne@gmail.com>
2024-04-17 11:57:48 +0000

    add: mocking for tests

--- OrderService/src/test/java/com/ebelemgnegre/OrderService/service/OrderServiceImplTest.java
+++ OrderService/src/test/java/com/ebelemgnegre/OrderService/service/OrderServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.ebelemgnegre.OrderService.service;
 
 import com.ebelemgnegre.OrderService.entity.Order;
+import com.ebelemgnegre.OrderService.entity.PaymentMode;
 import com.ebelemgnegre.OrderService.external.client.PaymentService;
 import com.ebelemgnegre.OrderService.external.client.ProductService;
+import com.ebelemgnegre.OrderService.external.response.PaymentResponse;
+import com.ebelemgnegre.OrderService.external.response.ProductResponse;
 import com.ebelemgnegre.OrderService.repository.OrderRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -43,7 +46,32 @@ class OrderServiceImplTest {
         Order order = getMockOrder();
         when(orderRepository.findById((anyLong())))
                 .thenReturn(Optional.of(order));
-        when(restTemplate.)
+        when(restTemplate.getForObject("http://PRODUCT-SERVICE/product/" + order.getProductId(),
+                ProductResponse.class)).thenReturn(getMockProductResponse());
+        when(restTemplate.getForObject("http://PAYMENT-SERVICE/payment/order/" + order.getId(),
+                PaymentResponse.class)).thenReturn(getMockPaymentResponse());
+        orderService.getOrderDetails(1);
+     }
+
+    private PaymentResponse getMockPaymentResponse() {
+      return PaymentResponse.builder()
+                .paymentId(1)
+                .orderId(1)
+                .amount(200)
+                .paymentMode(PaymentMode.CASH)
+                .paymentDate(Instant.now())
+                .paymentDate(Instant.now())
+                .status("ACCEPTED")
+                .build();
+    }
+
+    private ProductResponse getMockProductResponse() {
+        return ProductResponse.builder()
+                .productId(2)
+                .productName("iPhone 12 Pro Max")
+                .price(100)
+                .quantity(200)
+                .build();
     }
 
     private Order getMockOrder() {
