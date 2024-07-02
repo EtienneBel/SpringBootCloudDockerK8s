34122cb8ab1b61634375eed621e039309d8045bb
Wend-Kuuni <belemgnegreetienne@gmail.com>
2024-07-02 12:35:27 +0000

    adding k8s as service registry

--- OrderService/src/main/java/com/ebelemgnegre/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/ebelemgnegre/OrderService/external/client/ProductService.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @CircuitBreaker(name = "external", fallbackMethod = "fallback")
-@FeignClient(name = "PRODUCT-SERVICE/product")
+@FeignClient(name = "product", url = "${microservice.product}")
 public interface ProductService {
 
     @PutMapping("/reduceQuantity/{id}")
